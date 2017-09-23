dc6a70089ed2419c964292e4465cd09c031b3818
Abdenbi Ouammou <bassmatech9@gmail.com>
2017-09-23 10:19:56 +0100

    StringType
    Learning The Escape Characters And how to use it

---
+++ StringType/StringType/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // \' \" \\ \0 \b \n \r \t \v \f This is the escape characters
+
+            // @
+
+
+            string myString = "This is my string variable";
+            string myString2 = "Hassan Said \"This is the best Cours\" ";
+            string myString3 = @"C:\Users\Pc\source\repos";
+            string myString4 = myString2 + myString3; // Add string to Other
+
+            Console.WriteLine(" {0} \n {1}", myString, myString2);
+            Console.WriteLine("{0}", myString3);
+            Console.WriteLine("{0}", myString4);
+
+            /*
+             
+            Q1: How can I use the Character @ instead of the \ in the string to get same result
+                "Nazar has said \"This is a unique course \", And i think so";
+            Q2: What is the purpose of these escape characters \t \v \f 
+
+             */
+
+            Console.WriteLine("---------------------------- Exercice ------------------------------");
+
+            string Q1 = @"Nazar has said ""This is a unique course"",And i think";
+            string Q2 = "\t \v \f";
+            Console.WriteLine("This the Answer : {0}", Q2);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
