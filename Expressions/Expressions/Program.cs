52944355b7a3524d20236ed6fb557f4f2993a39c
Abdenbi Ouammou <bassmatech9@gmail.com>
2017-09-23 10:40:34 +0100

    Expressions
    In this lesson we will learn how to deal with expressions and how to get expression and the difference beween Expression, Operand and Operator

---
+++ Expressions/Expressions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float myVar = 10.5f;
+            float myVar2 = 10.7f;
+
+            float temp = myVar + myVar2;
+
+            //Basic Operators : + - /  *
+            //bitwise operations
+            //Logical Operators : (Boolean : True or False)
+
+            //1- Unary operand
+            //2- Binary Operands
+            //3- Tirnary operands
+
+
+
+        }
+    }
+}
