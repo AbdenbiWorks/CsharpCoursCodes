3e631cd2ba9c25466cebada4e59b9a1d8fdb1bfd
Abdenbi Ouammou <abdenbi.ou@outlook.com>
2017-09-24 19:14:52 +0100

    Assignement Operations
    In this lesson we will learn how to assign values to variables and how to deal with them and how to merge processes with each other

---
+++ Assignement Operations/Assignement Operations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignement_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // =, +=, *=, /=, %=
+
+            int myVar1 = 7, myVar2 = 2;
+
+            //myVar1 = myVar2;
+            //myVar1 += myVar2; // ==> myVar1 = myVar1 + myVar2
+            //myVar1 -= myVar2; // ==> myVar1 = myVar1 - myVar2
+            //myVar1 /= myVar2; // ==> myVar1 = myVar1 / myVar2
+            //myVar1 *= myVar2; // ==> myVar1 = myVar1 * myVar2
+            //myVar1 %= myVar2; // ==> myVar1 = myVar1 % myVar2
+
+            myVar1 /= ++myVar2;
+            Console.WriteLine("The value of myVar1 = " + myVar1);
+            Console.WriteLine("The value of myVar2 = " + myVar2);
+            Console.WriteLine("------------------------Exercice-------------------------");
+
+            /*
+              
+             Q1 / What's the result of this expression
+                  int temp, myVar1 = 7, myVar2 = 2;
+
+                 myVar1 /= ++myVar2;
+                        and why?
+             
+              
+             */
+
+            Console.ReadKey();
+        }
+    }
+}
