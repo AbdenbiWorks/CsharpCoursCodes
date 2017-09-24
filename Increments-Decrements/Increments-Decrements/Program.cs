587184c1623872ddeae5d7c178c71c7e6c7b1de5
Abdenbi Ouammou <abdenbi.ou@outlook.com>
2017-09-24 13:31:53 +0100

    Increments - Decrements
    The difference for built-in types is:
    *++a first increments the value of a and then returns an lvalue referring to a, so if the value of a is used then it will be the incremented value.
    *a++ first returns an rvalue whose value is a, that is, the old value, and then increments a at an unspecified time before the next full-expression (i.e., "before the semicolon").
    *Postfix increment has higher precedence than prefix increment.
    For user-defined types:
    *The same precedence rules apply; postfix increment has higher precedence than prefix increment.
    *++a calls a.operator++() or operator++(a). The semantics are determined by the implementation of the function. Whether it returns an lvalue or rvalue (or even any value at all) is up to the programmer.
    *a++ calls a.operator++(0) or operator++(a, 0). Again, the semantics are determined by the implementation of the function. Any modifications to a made inside the function will take effect before operator++ returns so by the time the rest of the expression sees the value of a++ the incrementation will have occurred already. This is a stronger guarantee than provided by the built-in operator.

---
+++ Increments-Decrements/Increments-Decrements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Increments_Decrements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 = 2;
+            int temp1 = myVar1++;
+
+            /*
+                            Cas1 : ++myVar1 first increments the value of a and then returns an lvalue referring to a, so if the value of a is used then it will be the incremented value.
+                            Cas2 : myVar1++ first returns an rvalue whose value is a, that is, the old value, and then increments a at an unspecified time before the next full-expression
+                                    (i.e., "before the semicolon")
+            */
+
+            Console.WriteLine();
+        }
+    }
+}
