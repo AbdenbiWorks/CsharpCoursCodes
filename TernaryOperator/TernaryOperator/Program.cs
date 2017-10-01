f0d3e776f1e3f707316083743ff8229348ee9b04
Abdenbi Ouammou <abdenbi.ou@outlook.com>
2017-10-01 13:44:09 +0100

    TernaryOperator

---
+++ TernaryOperator/TernaryOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TernaryOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar = 9;
+
+            string myResult = (myVar < 10) ? "less than 10" : "greater than 10"; 
+            Console.WriteLine("My Variable result is {0}", myResult);
+
+            string myName = "Abdenbi", result;
+            result = (myName.Equals("Abdenbi")) ? "yes" : "no";
+            Console.WriteLine("My Variable result is {0}", result);
+
+            Console.ReadKey();
+        }
+    }
+}
