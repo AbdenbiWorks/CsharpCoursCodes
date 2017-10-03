a32bb4567058585f787c570d6ac631014131ff5c
Abdenbi Ouammou <abdenbi.ou@outlook.com>
2017-10-03 14:43:27 +0100

    BreakLoop_ContinueCommand

---
+++ BreakLoop _ ContinueCommand/BreakLoop _ ContinueCommand/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakLoop___ContinueCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Hello Abdenbi");
+                if (i > 2)
+                {
+                    continue;
+                }
+                for (int j = 0; j<= 4; j++)
+                {
+                    Console.WriteLine("\t Hello World");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
