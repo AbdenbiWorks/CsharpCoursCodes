8203b402a49ef7b90a11b1e11c7a822bac44e75f
Abdenbi Ouammou <abdenbi.ou@outlook.com>
2017-10-04 16:45:52 +0100

    ExplicitConversion_Checked_Unchecked

---
+++ ExplicitConversion_Checked_Unchecked/ExplicitConversion_Checked_Unchecked/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitConversion_Checked_Unchecked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Type1 = (Type1) Type2                         //Casting
+
+            byte mySbyte;
+            int myInt = 350;
+
+            mySbyte = (byte)myInt;
+
+            Console.WriteLine($"My Variable Int = {myInt}" + "\n");
+            Console.WriteLine($"My byte Variable = {mySbyte}");
+            Console.WriteLine("------------------------------Exerceice-------------------------");
+
+            decimal myDecVar = 102524;
+            int Q17 = checked((int) myDecVar);
+
+            Console.WriteLine(Q17);
+
+
+
+            Console.ReadKey();
+
+            //Q17/ Define a Decimal variable and than convert it to an Integer using
+            //Either Implicit or Explicit conversion operations and use checked keyword to throught
+            // an exception of overflow type
+        }
+    }
+}
