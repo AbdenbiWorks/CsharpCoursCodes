8f426c15b2bdcbc8aeb70c5ba9e7db387ebbbcb9
Abdenbi Ouammou <abdenbi.ou@outlook.com>
2017-10-04 14:28:16 +0100

    Implicit_Conversion
    An important lesson we will learn about the transfers between species In this lesson we will learn the method of conversion between the types that allow the checker of the language of the Sharp by passing and not object to it .......

---
+++ Implicit_Conversion/Implicit_Conversion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implicit_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long myVar = 1000;
+            float myVar2 = myVar;
+            float myFloatVar = 12.5f;
+            int myInt2Var = myFloatVar;
+
+
+            Console.WriteLine(myVar2);
+
+            char myVar3 = 'G';
+            int myIntVar = myVar3;
+
+            Console.WriteLine(myIntVar);
+            Console.ReadKey();
+        }
+    }
+}
