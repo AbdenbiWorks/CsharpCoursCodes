429975aa61c001c5e125ac7719fd2adad109f298
Abdenbi Ouammou <abdenbi.ou@outlook.com>
2017-10-04 15:39:56 +0100

    ExplicitConversionType

---
+++ ExplicitConversionType/ExplicitConversionType/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitConversionType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Type1 = (Type1)Type2                      //Casting
+
+            byte mySByte;
+            int myInt = 7;
+            // mySByte = myInt;
+            mySByte = (byte)myInt;
+
+            Console.WriteLine($"My Variable Int = {myInt}" + "\n");
+            Console.WriteLine($"My Byte Variable = {mySByte}");
+            Console.ReadKey();
+        }
+    }
+}
