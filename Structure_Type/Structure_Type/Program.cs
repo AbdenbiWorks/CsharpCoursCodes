bfdd962bfc7631d647c9c10bcab6067a17b34b80
Abdenbi Ouammou <abdenbi.ou@outlook.com>
2017-10-07 11:48:58 +0100

    Structure_Type

---
+++ Structure_Type/Structure_Type/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structure_Type
+{
+    class Program
+    {
+        enum toyota : byte
+        {
+            RAV4        =1,
+            Camery      =2,
+            Tundra      =3,
+            HighLand    =4
+        }
+        struct carType{
+            public toyota modelName;
+            public double modelYear;
+        }
+        static void Main(string[] args)
+        {
+            carType myCarType;
+            int carType = -1;
+            double modelYear;
+            Console.WriteLine("1) RAV4\n2) Camery\n3) Tundra\n4) HighLand");
+        }
+    }
+}
