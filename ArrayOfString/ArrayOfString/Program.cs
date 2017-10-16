f4a8346e1278dfa8a8d07269ceffad2f02267dc9
Abdenbi Ouammou <abdenbi.ou@outlook.com>
2017-10-16 07:23:55 +0100

    ArrayOfString

---
+++ ArrayOfString/ArrayOfString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myFullName = { "Abdenbi", "Ouammou" };
+            int myArraylenght = myFullName.Length;
+
+            for (int i =0; i < myArraylenght; i++)
+            {
+                Console.Write($"{myFullName[i]} ");
+
+            }
+            Console.WriteLine($"\n{myArraylenght}");
+            Console.ReadKey();
+        }
+    }
+}
