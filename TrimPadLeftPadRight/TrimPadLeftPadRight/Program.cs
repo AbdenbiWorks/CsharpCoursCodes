2373c98f07b4ee0ea212f900e6448634a1072a8b
Abdenbi Ouammou <abdenbi.ou@outlook.com>
2017-10-23 15:55:57 +0100

    TrimPadLeftPadRight

---
+++ TrimPadLeftPadRight/TrimPadLeftPadRight/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrimPadLeftPadRight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString, myString2;
+
+
+            char[] mytrimChar = { 'h', 'e', 'i', ' ' };
+            Console.WriteLine("Please Enter Your Text");
+            myString = Console.ReadLine();
+            myString = myString.ToLower();
+            myString2 = myString.TrimStart(mytrimChar);
+            Console.WriteLine($"{myString2}");
+            myString2 = myString.TrimEnd(mytrimChar);
+            Console.WriteLine($"{myString2}");
+
+            myString2 = myString.PadLeft(25, '-');
+            Console.WriteLine($"{myString2}");
+            myString2 = myString.PadRight(25, '-');
+            Console.WriteLine($"{myString2}");
+            
+
+
+            Console.ReadKey();
+
+            //Q24
+            // Change this string from this style "Hello my Dear" to be
+            // in this style "hello My Dearest" using pads functions oly
+        }
+    }
+}
