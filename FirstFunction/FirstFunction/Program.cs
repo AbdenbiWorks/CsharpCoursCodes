b5eb34e5a4fea83c30f82ed0b676afd36e004180
Abdenbi Ouammou <abdenbi.ou@outlook.com>
2017-10-24 08:55:06 +0100

    First Function

---
+++ FirstFunction/FirstFunction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstFunction
+{
+    class Program
+    {
+        static void printName()
+        {
+            Console.Write("My Name is Abdenbi");
+        }
+        static void Main(string[] args)
+        {
+            printName();
+            Console.ReadKey();
+        }
+    }
+}
