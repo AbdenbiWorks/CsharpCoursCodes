ab2e0285959df22df1cdc2b2472630d2c0e3990d
Abdenbi Ouammou <abdenbi.ou@outlook.com>
2017-10-24 10:11:15 +0100

    MinimumValue Function and challenge 26 مثال على الدوال دالة ارجاع القيمة الأصغر والتحدي رقم

---
+++ ReturnValues/ReturnValues/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnValues
+{
+    class Program
+    {
+        static int MinValue(int[] myArray)
+        {
+            int minVal = myArray[0];
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] < minVal)
+                {
+                    minVal = myArray[i];
+                }
+            }
+            return minVal;
+        }
+
+        static double AverageStudent(double[] avrgArray)
+        {
+            double avrgVal = 0.0d;
+            for (int i = 0;i < avrgArray.Length; i++)
+            {
+                avrgVal = (avrgVal + avrgArray[i] / 7);
+            }
+            return avrgVal;
+
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = { 11, 5, 8, 6, 4, 15, 6, 84, 55, 99, 3, 5 };
+            int minVal = MinValue(myArray);
+            Console.WriteLine($"The minimum value in myArray is {minVal}");
+
+            Console.WriteLine("---------------Exercice---------------");
+            //Q26 Define a function to find the avarage
+            //of a student that has 7grades and print the result in the void maind
+
+            double[] avrgArray = {45, 1, 54, 55, 41, 5, 54 };
+            double avrgVal = AverageStudent(avrgArray);
+            Console.WriteLine($"The Average for 7 grades = {avrgVal} ");
+            Console.ReadKey();
+        }
+    }
+}
