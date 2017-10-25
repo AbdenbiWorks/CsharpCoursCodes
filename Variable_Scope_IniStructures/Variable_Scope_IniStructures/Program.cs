3e0588a628656d6789ebeb3f23d3776a9c0be68d
Abdenbi Ouammou <abdenbi.ou@outlook.com>
2017-10-25 16:13:00 +0100

    Variable Scope in Structures مدى المتغيرات داخل الهياكل البرمجية
    في هذا الدرس سنتعلم ماهو مدى المتغيرات في التراكيب البرمجية وكيفية أمكانية الوصول الى المتغيرات ولماذا بعض المتغيرات تكون محجوبة بلغة السي شارب

---
+++ Variable_Scope_IniStructures/Variable_Scope_IniStructures/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable_Scope_IniStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i < 10; i++)
+            {
+                int index = i;
+                Console.WriteLine($"{index}");
+
+            }
+            Console.WriteLine($"Last index output in loop : {index}"); //You have to declare the index variable and defined him
+            Console.ReadKey();
+        }
+    }
+}
