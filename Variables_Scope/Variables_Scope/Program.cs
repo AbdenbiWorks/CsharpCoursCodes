e1a490d24affca6eec755c2fcf84a6a783939122
Abdenbi Ouammou <abdenbi.ou@outlook.com>
2017-10-25 16:00:07 +0100

    Variables Scope مدى المتغيرات وامكانية الوصول

---
+++ Variables_Scope/Variables_Scope/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables_Scope
+{
+    class Program
+    {
+        static string myString;
+        static void PrintMyString()
+        {
+            string myString = "my string inside the printmyString method";
+            Console.WriteLine($"{myString}");
+            Console.WriteLine($"{Program.myString}");
+        }
+        static void Main(string[] args)
+        {
+            string myString = "my string inside the main method";
+            Program.myString = "Global my String Variable";
+            PrintMyString();
+            Console.WriteLine($"{myString}");
+            Console.WriteLine($"{Program.myString}");
+            Console.ReadKey();
+        }
+    }
+}
