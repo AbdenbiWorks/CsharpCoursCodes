90e5ddf521cff60db08179d3b3986c7f9ddb2e2a
Abdenbi Ouammou <abdenbi.ou@outlook.com>
2017-10-26 15:43:09 +0100

    Struct Function دوال الهيكل البرمجي
    في هذا الدرس سنتعلم طريقة التعامل مع دوال الهيكل البرمجي الـــتStructure ببرنامج السي شارب وماهي الأمور المهمة التي من الواجب عليك معرفتها عن هذه الدالة المهمة وطريقة التعامل معها بلغة السي شارب

---
+++ StructFunctions/StructFunctions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructFunctions
+{
+    class Program
+    {
+        struct FullName
+        {
+            public string myFirstName, myLastName;
+            public string printName() => myFirstName + " " + myLastName;
+        }
+        static void Main(string[] args)
+        {
+            FullName myFullName;
+            myFullName.myFirstName = "Abdenbi";
+            myFullName.myLastName = "Ouammou";
+            //Console.Write($"{myFullName.myFirstName} {myFullName.myLastName}");
+            Console.Write($"{myFullName.printName()}");
+            Console.ReadKey();
+        }
+    }
+}
