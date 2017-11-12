e29fcf6e5bce4dc8ae69e28e1dca37de85994b75
Abdenbi Ouammou <abdenbi.ou@outlook.com>
2017-11-12 16:57:27 +0000

    AssertMethod
    في هذا الدرس سنتعلم طريقة التعامل مع واحدة من الدوال المهمة لتدقيق الكود البرمجي وهي دوال الــــAssertion Methods
    داخل الكود البرمجي المسيطر عليها من قبل المبرمج لتتبع الكود خطوة بخطوة للبرنامج وماهي الأمور المهمة التي من الواجب عليك معرفتها عن هذه النوعية من عمليات التدقيق وطريقة تعاملها مع لغة السي شارب

---
+++ AssertMethod/AssertMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Diagnostics;
+
+namespace AssertMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Enter a value");
+            int i = Convert.ToInt32(Console.ReadLine());
+            if(i >= 0)
+            {
+                Console.WriteLine($"{i}");
+            }
+            else
+            {
+                Debug.WriteLine("We Are Here");
+                Debug.Assert(i >= 100, "Short", "Long");
+            }
+            WriteLine("Done");
+            ReadKey();
+        }
+    }
+}
